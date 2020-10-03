f45f192d32a219966d61bf2d3113be581ffb0309
chaitanya <kc508275@gmail.com>
2020-10-03 14:07:58 +0530

    form working

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Home(models.Model):
+    name = models.CharField(max_length=30,null=True)
+    email = models.EmailField(max_length=20,null=True)
+    subject1 = models.TextField(null=True)
+    message1 =models.TextField(null=True)
+    def __str__(self):
+                return self.subject1
--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render,HttpResponse
-
+from home.models import Home
+from  django.contrib import messages
 # Create your views here.
 def home(request):
+    if request.method =="POST":
+            name = request.POST['name']
+            email = request.POST['email']
+            subject = request.POST['subject']
+            message = request.POST['message']
+            ins = Home(name=name,email=email, subject1=subject, message1=message)
+            ins.save()
+            messages.success(request,'u r messege has been recived')
+            print("done for the data")
     return render(request,'index.html')
 def sample(request):
     return render(request,'sample.html')
